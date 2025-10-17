f6a442252332c44a0ca568d8957584c012851b85
Warwlock <73683644+Warwlock@users.noreply.github.com>
2025-10-17 17:10:35 +0300

    Custom Noise Job Test

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,30 +1,36 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using NoiseDotNet;
+using System;
+using Unity.Jobs;
 
 public class Chunk : MonoBehaviour
 {
-    static int3 chunkSize = new int3(16, 16, 384);
+    static int3 chunkSize = new int3(16, 384, 16);
 
     int2 position = new int2(0, 0);
 
     List<Voxel> voxelData = new List<Voxel>();
 
-    float[] xCoords = new float[chunkSize.x * chunkSize.y];
+    float[] xCoords = new float[chunkSize.x * chunkSize.z];
 
-    float[] yCoords = new float[chunkSize.x * chunkSize.y];
+    float[] yCoords = new float[chunkSize.x * chunkSize.z];
 
-    float[] output = new float[chunkSize.x * chunkSize.y];
+    float[] output = new float[chunkSize.x * chunkSize.z];
 
     float GetValueAt(int x, int y)
     {
-        return output[y * chunkSize.y + x];
+        return output[y * chunkSize.z + x];
     }
+
+    JobHandle noiseJobHandle;
+    bool isGenerated;
 
     void Start()
     {
         int index = 0;
-        for (int y = 0; y < chunkSize.y; y++)
+        for (int y = 0; y < chunkSize.z; y++)
             for (int x = 0; x < chunkSize.x; x++)
             {
                 xCoords[index] = x;
@@ -32,30 +38,59 @@ public class Chunk : MonoBehaviour
                 index++;
             }
 
-        NoiseDotNet.Noise.GradientNoise2D(
-            xCoords: xCoords,
-            yCoords: yCoords,
-            output: output,
-            xFreq: 0.1f, // x-coordinates are multiplied by this value before being used
-            yFreq: 0.1f, // y-coordinates are multiplied by this value before being used
-            amplitude: 1f, // the result of the noise function is multiplied by this value
-            seed: 100);
+        var noiseJob = CreateGradient2DNoiseJob(xCoords, yCoords, output, 0.1f, 0.1f, 1f, 100);
+        noiseJobHandle = noiseJob.Schedule();
+    }
 
-        for (int i = 0; i < chunkSize.x * chunkSize.y; i++)
+    unsafe BurstNoiseJob CreateGradient2DNoiseJob(ReadOnlySpan<float> x, ReadOnlySpan<float> y, Span<float> output, float xFreq, float yFreq, float amplitude, int seed)
+    {
+        if (output.Length == 0)
+            throw new ArgumentException($"Output buffer length was 0. Expected > 0.");
+        if (output.Length != x.Length)
+            throw new ArgumentException($"Expected x buffer length {x.Length} to equal output buffer length {output.Length}");
+        if (output.Length != y.Length)
+            throw new ArgumentException($"Expected y buffer length {y.Length} to equal output buffer length {output.Length}");
+
+        fixed (float* xPtr = x)
         {
-            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            float height = Mathf.CeilToInt(output[i] * 10f);
-            obj.transform.position = new Vector3(xCoords[i], height, yCoords[i]);
-
-            var col = output[i];
-
-            obj.GetComponent<MeshRenderer>().material.color = new Color(col, col, col);
+            fixed (float* yPtr = y)
+            {
+                fixed (float* outPtr = output)
+                {
+                    BurstNoiseJob job = new();
+                    job.noiseType = NoiseType.GradientNoise2D;
+                    job.xBuffer = xPtr;
+                    job.yBuffer = yPtr;
+                    job.output1Buffer = outPtr;
+                    job.length = x.Length;
+                    job.amplitude1 = amplitude;
+                    job.xFrequency = xFreq;
+                    job.yFrequency = yFreq;
+                    job.seed = seed;
+                    return job;
+                }
+            }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (noiseJobHandle.IsCompleted && !isGenerated)
+        {
+            noiseJobHandle.Complete();
+            isGenerated = true;
 
+            for (int i = 0; i < chunkSize.x * chunkSize.z; i++)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                float height = Mathf.CeilToInt(output[i] * 10f);
+                obj.transform.position = new Vector3(xCoords[i], height, yCoords[i]);
+
+                var col = output[i];
+
+                obj.GetComponent<MeshRenderer>().material.color = new Color(col, col, col);
+            }
+            noiseJobHandle = CreateGradient2DNoiseJob(xCoords, yCoords, output, 0.1f, 0.1f, 1f, 100).Schedule();
+        }
     }
 }
